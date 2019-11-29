89f81df94cc897e8007a89556702adb2c0b6cbee
lucas-maijers <lucas.maijers@student.rocvf.nl>
2019-11-29 09:05:25 +0100

    op2

---
+++ op2/nogeenindex.php
@@ -0,0 +1,8 @@
+<?php
+
+$a = "ry";
+$b = ", but";
+$c = "fail";
+
+echo "t" . $a . " and " . $c . $b . " never " . $c . " to t" . $a;
+
---
+++ op2/nogeentje.php
@@ -0,0 +1,8 @@
+<?php
+
+$a = "die geen fout";
+$b = "en";
+$c = "niet.";
+
+echo "Mens" . $b . " " . $a . $b . " mak" . $b . ", werk" . $b . " ".$c;
+
